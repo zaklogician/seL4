f7716439b75187f7e090aabf92083fdd0ff7e236
James Ye <james.ye@data61.csiro.au>
2019-05-24 16:41:00 +1000

    odroidc2: watchdog reset for AOS
    Reset when "reset" is typed into UART.
    
    Co-Authored-By: Anna Lyons <Anna.Lyons@data61.csiro.au>
    Co-Authored-By: Curtis Millar <Curtis.Millar@data61.csiro.au>

--- include/arch/arm/arch/object/interrupt.h
+++ include/arch/arm/arch/object/interrupt.h
@@ -19,6 +19,11 @@ exception_t Arch_decodeIRQControlInvocation(word_t invLabel, word_t length,
 /* Handle a platform-reserved IRQ. */
 static inline void handleReservedIRQ(irq_t irq)
 {
+#ifdef CONFIG_IRQ_REPORTING
+    if (IRQT_TO_IRQ(irq) != KERNEL_UART_IRQ) {
+        printf("Received reserved IRQ: %d\n", (int)IRQT_TO_IRQ(irq));
+    }
+#endif
 
 #ifdef CONFIG_ARM_ENABLE_PMU_OVERFLOW_INTERRUPT
     if (IRQT_TO_IRQ(irq) == KERNEL_PMU_IRQ) {
@@ -49,6 +54,11 @@ static inline void handleReservedIRQ(irq_t irq)
 #ifdef CONFIG_IRQ_REPORTING
     printf("Received unhandled reserved IRQ: 0x%lx\n", IRQT_TO_IRQ(irq));
 #endif
+
+    if (IRQT_TO_IRQ(irq) == KERNEL_UART_IRQ) {
+        handleUartIRQ();
+        return;
+    }
 }
 
 
